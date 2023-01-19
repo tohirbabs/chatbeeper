12cfae01b329c26ad39fc6bdbc20934952a34af3
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-01-19 13:43:50 +0100

    index

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,3 +1,2 @@
 export { default as SignUpForm } from "./SignUpForm";
-// export { default as OtherDetails } from "./OtherDetails";
-// export { default as Username } from "./Username";
+export { default as LoginForm } from "./LoginForm";
