23132fcf7436aa40dbd359fa8c69c784c14d94e7
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-01-20 17:13:52 +0100

    headaer updates

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,260 @@
+import * as React from "react";
+import chatbeeper from "../../assets/chatbeeper.png";
+
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import ChatBeeperLogo from "../Logo";
+import { BeepIcon, HomeIcon, NotificationIcon, TourIcon } from "../icons";
+import { Avatar, Divider, Tooltip } from "@mui/material";
+const drawerWidth = 260;
+
+const ChatbeeperText = styled("div")(({ theme }) => ({
+  position: "relative",
+  marginRight: theme.spacing(2),
+  marginLeft: theme.spacing(2),
+  width: "100%",
+  display: "flex",
+  justifyContent: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+export default function HeaderBar({ handleDrawerToggle }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Box>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px  - 100vw + 1200px )` },
+          ml: { sm: `calc(${drawerWidth}px + 50vw - 600px)` },
+          mr: { sm: `calc(50vw - 600px)` },
+        }}
+        elevation={0}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <AccountCircle />
+          </IconButton>
+          <ChatbeeperText>
+            <Box component="img" src={chatbeeper} sx={{ width: "60%" }} />
+          </ChatbeeperText>
+
+          <Search
+            sx={{ borderRadius: "1rem", display: { xs: "none", md: "block" } }}
+          >
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+              sx={{ width: { md: "350px" } }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Tooltip title="Home">
+              <IconButton size="large" aria-label="Home" color="inherit">
+                <HomeIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Tour">
+              <IconButton size="large" aria-label="Tour" color="inherit">
+                <TourIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Beep">
+              <IconButton size="large" aria-label="Beep" color="inherit">
+                <BeepIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Notifications">
+              <IconButton
+                size="large"
+                aria-label="Notifications"
+                color="inherit"
+              >
+                <Badge badgeContent={4} color="error">
+                  <NotificationIcon />
+                </Badge>
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Profile">
+              <IconButton size="large" aria-label="Profile" color="inherit">
+                <AccountCircle />
+
+                {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
+              </IconButton>
+            </Tooltip>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton size="large" aria-label="Beep" color="inherit">
+              <SearchIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+        <Divider />
+      </AppBar>
+
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  );
+}
