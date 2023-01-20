9f3d3b4fcbfc0be00fcd143fe271ce00cd5c3fba
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-01-20 17:08:02 +0100

    index

---
+++ src/components/icons/index.js
@@ -0,0 +1,12 @@
+export { default as HomeIcon } from "./HomeIcon";
+export { default as BeepIcon } from "./BeepIcon";
+export { default as TourIcon } from "./TourIcon";
+export { default as NotificationIcon } from "./NotificationIcon";
+export { default as SettingsIcon } from "./SettingsIcon";
+export { default as TrendIcon } from "./TrendIcon";
+export { default as FollowersIcon } from "./FollowersIcon";
+export { default as BriefcaseIcon } from "./BriefcaseIcon";
+export { default as BookmarkIcon } from "./BookmarkIcon";
+export { default as SmsIcon } from "./SmsIcon";
+export { default as LogoutIcon } from "./LogoutIcon";
+export { default as SponsoredIcon } from "./SponsoredIcon";
