672d0f93dbc59420724e0877e0d7bfcbf96cc0b5
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-01-21 15:32:06 +0100

    layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
-import { Sidebar } from "../Sidebar/Sidebar";
-import { Header } from "../Header/Header";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+
+import Container from "@mui/material/Container";
+import { pageAnimation } from "../../animations";
+import { Logo } from "../../assets/logo";
 import "./style.css";
 import { FooterMenu } from "../FooterMenu/FooterMenu";
 import { Overlay } from "../Overlay/Overlay";
@@ -13,14 +18,36 @@ import HeaderBar from "./Header";
 import Footer from "./Footer";
 import SideDrawer from "./Sidebar";
 import { Box, Toolbar } from "@mui/material";
+import toast from "react-hot-toast";
+import { GET } from "../../utils/request";
 
 export const Layout = ({ children, setHomeFeedData, homeFeedData }) => {
   const [sidebar, setSidebar] = useState("");
   const [overlay, setOverlay] = useState("");
   const [addBeep, setAddBeep] = useState(false);
 
-  const userInfo = useStore((state) => state.userReg.auth);
+  const [loading, setloading] = useState(true);
 
+  const updateUserData = useStore((state) => state.updateUserData);
+  const updateAvatar = useStore((state) => state.updateAvatar);
+  const userData = useStore((state) => state.userData);
+  const userAvatar = useStore((state) => state.userAvatar);
+
+  console.log(userData);
+
+  useEffect(() => {
+    GET("user", userInfo.jwt)
+      .then((res) => res.json())
+      .then(
+        (res) =>
+          (res._id && updateUserData(res)) ||
+          (res.message && toast(res.message))
+      )
+      .catch((err) => console.log("error:", err))
+      .finally(() => setloading(false));
+  }, []);
+
+  const userInfo = useStore((state) => state.userReg.auth);
   const location = useLocation();
   if (!userInfo) {
     location("/ceate-account");
@@ -35,41 +62,69 @@ export const Layout = ({ children, setHomeFeedData, homeFeedData }) => {
   console.log(mobileOpen);
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        position: "relative",
-        width: { lg: "1200px" },
-        margin: "auto",
-      }}
-    >
-      <HeaderBar
-        handleDrawerToggle={handleDrawerToggle}
-        setAddBeep={setAddBeep}
-      />
-      <SideDrawer
-        handleDrawerToggle={handleDrawerToggle}
-        mobileOpen={mobileOpen}
-      />
+    <>
+      {loading ? (
+        <motion.div
+          variants={pageAnimation}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transitions={pageAnimation.transition}
+        >
+          <Container
+            sx={{
+              display: "grid",
+              placeItems: "center",
+              height: "100vh",
+            }}
+          >
+            <Box
+              component="img"
+              alt="chatbeeper logo"
+              src={Logo}
+              width={80}
+              height={80}
+            />
+          </Container>
+        </motion.div>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            position: "relative",
+            width: { lg: "1200px" },
+            margin: "auto",
+          }}
+        >
+          <HeaderBar
+            handleDrawerToggle={handleDrawerToggle}
+            setAddBeep={setAddBeep}
+          />
+          <SideDrawer
+            handleDrawerToggle={handleDrawerToggle}
+            mobileOpen={mobileOpen}
+          />
 
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
 
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-        }}
-      >
-        <Toolbar />
-        {children}
-      </Box>
-      {/* <FooterMenu
+              width: { sm: `calc(100% - ${drawerWidth}px)` },
+            }}
+          >
+            <Toolbar />
+            {children}
+          </Box>
+          {/* <FooterMenu
         nav={location.pathname}
         setAddBeep={setAddBeep}
         overlay={overlay}
       /> */}
-      <Footer setAddBeep={setAddBeep} />
-      <BeepPrompt open={addBeep} setAddBeep={setAddBeep} />
-    </Box>
+          <Footer setAddBeep={setAddBeep} />
+          <BeepPrompt open={addBeep} setAddBeep={setAddBeep} />
+        </Box>
+      )}
+    </>
   );
 };
