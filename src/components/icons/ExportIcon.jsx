2b5e77f31cee70f51606638ccd93a0a61313fc03
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-01-21 13:36:32 +0100

    new icons

---
+++ src/components/icons/ExportIcon.jsx
@@ -0,0 +1,33 @@
+export default function ExportIcon() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M21.92 11.8667C26.72 12.28 28.68 14.7467 28.68 20.1467V20.32C28.68 26.28 26.2933 28.6667 20.3333 28.6667H11.6533C5.69331 28.6667 3.30664 26.28 3.30664 20.32V20.1467C3.30664 14.7867 5.23997 12.32 9.95997 11.88"
+        stroke="#11CDF3"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M16 20V4.82666"
+        stroke="#11CDF3"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M20.4667 7.79992L16 3.33325L11.5333 7.79992"
+        stroke="#11CDF3"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ src/components/icons/MessageIcon.jsx
@@ -0,0 +1,41 @@
+export default function MessageIcon() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.3333 25.3334H10.6667C5.33333 25.3334 2.66667 24.0001 2.66667 17.3334V10.6667C2.66667 5.33341 5.33333 2.66675 10.6667 2.66675H21.3333C26.6667 2.66675 29.3333 5.33341 29.3333 10.6667V17.3334C29.3333 22.6667 26.6667 25.3334 21.3333 25.3334H20.6667C20.2533 25.3334 19.8533 25.5334 19.6 25.8667L17.6 28.5334C16.72 29.7067 15.28 29.7067 14.4 28.5334L12.4 25.8667C12.1867 25.5734 11.6933 25.3334 11.3333 25.3334Z"
+        stroke="#11CDF3"
+        stroke-width="2"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M21.3286 14.6667H21.3406"
+        stroke="#11CDF3"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M15.994 14.6667H16.006"
+        stroke="#11CDF3"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10.6593 14.6667H10.6713"
+        stroke="#11CDF3"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
